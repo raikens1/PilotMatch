1a0f25198f99dedec2b8076163efa016f68bcad2
EC2 Default User <ec2-user@ip-172-31-50-38.ec2.internal>
2019-04-11 01:17:02 +0000

    fixing simulations

--- code/Simulation_Batch_Driver.sh
+++ code/Simulation_Batch_Driver.sh
@@ -2,7 +2,7 @@
 
 NSIM=$1
 
-for i in {1..10}; do
+for i in {0..10}; do
   RHO=$(echo "scale = 1; ${i}/10" | bc);
   DATE=`date +%Y-%m-%d`
   nohup Rscript ../code/simulate_wrapper.R ${RHO} 10 ${NSIM} >> ${i}_sim_${DATE}.log &
