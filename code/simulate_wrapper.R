0dfb6b84a48c161b6af1ed79b6dbe40524a48d13
raikens1 <rockyaikens@gmail.com>
2019-04-16 11:28:39 -0700

    set verbose = TRUE in wrapper

--- code/simulate_wrapper.R
+++ code/simulate_wrapper.R
@@ -22,7 +22,7 @@ p <- as.numeric(args[2])
 nsim <- as.numeric(args[3])
 
 # simulate and write to file
-results <- replicate(nsim, simulate(generate_data(rho=rho, p = p)),
+results <- replicate(nsim, simulate(generate_data(rho=rho, p = p, verbose = TRUE)),
                      simplify = FALSE) %>% 
   bind_rows
 write.csv(results, file = paste0(out_file, rho*10, "_", p, "_", nsim), row.names = FALSE)
