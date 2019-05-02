839dec49e6902a9894e22d1cc729a36b27aa5d4c
raikens1 <rockyaikens@gmail.com>
2019-05-02 16:28:38 -0700

    added tau =0.5 default to wrapper

--- code/simulate_wrapper.R
+++ code/simulate_wrapper.R
@@ -4,7 +4,7 @@
 #' nsim (int) number of simulations
 
 
-source("../code/simulation_functions.R")
+source("simulation_functions.R")
 
 out_file <- "angle_sigma1_results_"
 
@@ -16,10 +16,10 @@ rho <- as.numeric(args[1])
 p <- as.numeric(args[2])
 nsim <- as.numeric(args[3])
 
-run_sim <- function(rho = 0.1, p = 10, nsim = 10, out_file = "test_", true_mu = "X1/3 - 3", ks = 1:10, sigma = 1) {
+run_sim <- function(rho = 0.1, p = 10, nsim = 10, out_file = "test_", true_mu = "X1/3 - 3", ks = 1:10, sigma = 1, tau = 0.5) {
   t1 <- proc.time()
   # simulate
-  results <- replicate(nsim, simulate(generate_data(rho=rho, p = p, true_mu = true_mu, sigma = sigma),
+  results <- replicate(nsim, simulate(generate_data(rho=rho, p = p, true_mu = true_mu, sigma = sigma, tau = tau),
                                       verbose = TRUE, ks = ks),
                        simplify = FALSE) %>% 
     bind_rows()
