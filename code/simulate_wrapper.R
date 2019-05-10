cd0c24ab686963f5491ad82026bae2af0c9723cd
EC2 Default User <ec2-user@ip-172-31-50-38.ec2.internal>
2019-05-10 20:14:18 +0000

    reran tau = 1/2 sims with correct mu

--- code/simulate_wrapper.R
+++ code/simulate_wrapper.R
@@ -16,10 +16,10 @@ rho <- as.numeric(args[1])
 p <- as.numeric(args[2])
 nsim <- as.numeric(args[3])
 
-run_sim <- function(rho = 0.1, p = 10, nsim = 10, out_file = "test_", true_mu = "X1/3 - 3", ks = 1:10, sigma = 1, tau = 0.5) {
+run_sim <- function(rho = 0.1, p = 10, nsim = 10, out_file = "test_", true_mu = "X1 - 10/3", ks = 1:10, sigma = 1, tau = 0.5, N = 2000) {
   t1 <- proc.time()
   # simulate
-  results <- replicate(nsim, simulate(generate_data(rho=rho, p = p, true_mu = true_mu, sigma = sigma, tau = tau),
+  results <- replicate(nsim, simulate(generate_data(N=N, rho=rho, p = p, true_mu = true_mu, sigma = sigma, tau = tau),
                                       verbose = TRUE, ks = ks),
                        simplify = FALSE) %>% 
     bind_rows()
@@ -49,4 +49,4 @@ run_sim <- function(rho = 0.1, p = 10, nsim = 10, out_file = "test_", true_mu = 
 
 }
 
-run_sim(rho = rho, p = p, nsim = nsim, out_file = out_file, sigma = 1, ks = 1:10)
+run_sim(rho = rho, p = p, nsim = nsim, out_file = out_file, sigma = 1, ks = 1:10, N = 2000)
