a9d5c0acb6bf4d3be014bda891162ef107107cd9
raikens1 <rockyaikens@gmail.com>
2020-02-21 13:37:35 -0800

    set up parameters for SITA sims

--- code/SITA_violation_sim_wrapper.R
+++ code/SITA_violation_sim_wrapper.R
@@ -22,10 +22,10 @@ nsim <- as.numeric(args[3])
 true_mu <- "X1/3 - 3"
 sigma <- 1
 tau <- 1
-ks <- c(1,2,3,5)
+ks <- 1:10
 N <- 2000
 full <- F
-nu <- 0.4
+nu <- 0.2
 prop_model <- formula(t ~ . - mu - y - U) 
 prog_model <- formula(y ~ . - mu - t - U)
 
